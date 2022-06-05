1bc85a906f3dd4e6b8669d9055ee3e908bcc0f30
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-06-05 18:18:46 +0530

    Initial Setup

---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '/src/index.html',
+      title: 'Battleship App',
+    }),
+  ],
+};
