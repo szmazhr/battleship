a6d26e20dbe82f4fa593737e760c398ada8eda22
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-06-06 09:58:25 +0530

    Added GameBoard and test

---
+++ src/js/factories/game-board.js
@@ -0,0 +1,167 @@
+import Ship from './ship';
+
+function createBoard(x, y) {
+  const b = [];
+  for (let i = 0; i < y; i++) {
+    b.push([]);
+    for (let j = 0; j < x; j++) {
+      b[i].push(0);
+    }
+  }
+  return b;
+}
+
+function GameBoard(x, y) {
+  this.board = createBoard(x, y);
+  this.trackShot = createBoard(x, y);
+  this.ships = [];
+}
+
+// Prevent from being placed outside the board
+function isValidCell(cordX, cordY) {
+  return (
+    cordX >= 0 &&
+    cordX < this.board[0].length &&
+    cordY >= 0 &&
+    cordY < this.board.length
+  );
+}
+
+// Prevent from being placed ontop of another ship
+function isCellEmpty(cordX, cordY) {
+  return this.board[cordY][cordX] === 0;
+}
+
+// prevent from being placed on others neighbors
+function isPlacementPossible(cordX, cordY, length, axis) {
+  const cords = [];
+  let result = true;
+  const start = axis === 'y' ? cordY : cordX;
+  const end = axis === 'y' ? cordY + length : cordX + length;
+  for (let j = -1; j < 2; j++) {
+    for (let i = start - 1; i < end + 1; i++) {
+      if (axis === 'y') {
+        if (this.isValidCell(cordX + j, i)) {
+          cords.push([cordX + j, i]);
+        }
+      } else if (axis === 'x') {
+        if (this.isValidCell(i, cordY + j)) {
+          cords.push([i, cordY + j]);
+        }
+      }
+    }
+  }
+  cords.forEach((cord) => {
+    if (!this.isCellEmpty(cord[0], cord[1])) {
+      result = false;
+    }
+  });
+  return result;
+}
+
+function addShip(length, cordX, cordY, axis) {
+  // prevent ship from being placed outside the board
+  if (!this.isValidCell(cordX, cordY)) return;
+
+  let cX = cordX;
+  let cY = cordY;
+
+  if (axis === 'y') {
+    // shift the coordinates if target cell is valid but
+    // placement invalid due to length
+    if (cY + length > this.board.length) {
+      cY = this.board.length - length;
+    }
+    // check if placement is possible
+    if (!this.isPlacementPossible(cX, cY, length, axis)) return;
+
+    // add ship to board
+    for (let i = 0; i < length; i++) {
+      this.board[cY + i][cX] = 1;
+    }
+  } else if (axis === 'x') {
+    // shift the coordinates if target cell is valid but
+    // placement invalid due to length
+    if (cX + length > this.board[cY].length) {
+      cX = this.board[cY].length - length;
+    }
+    // check if placement is possible
+    if (!this.isPlacementPossible(cX, cY, length, axis)) return;
+
+    // add ship to board
+    for (let i = 0; i < length; i++) {
+      this.board[cY][cX + i] = 1;
+    }
+  }
+
+  const s = {
+    ship: new Ship(length),
+    cX,
+    cY,
+    axis,
+  };
+
+  this.ships.push(s);
+}
+
+// prevent shots corner neighbors if ship hits
+function recordHit(cordX, cordY) {
+  if (!this.isValidCell(cordX, cordY)) return;
+  this.trackShot[cordY][cordX] = 2;
+
+  if (this.isValidCell(cordX - 1, cordY - 1)) {
+    this.trackShot[cordY - 1][cordX - 1] = -1;
+  }
+  if (this.isValidCell(cordX - 1, cordY + 1)) {
+    this.trackShot[cordY + 1][cordX - 1] = -1;
+  }
+  if (this.isValidCell(cordX + 1, cordY - 1)) {
+    this.trackShot[cordY - 1][cordX + 1] = -1;
+  }
+  if (this.isValidCell(cordX + 1, cordY + 1)) {
+    this.trackShot[cordY + 1][cordX + 1] = -1;
+  }
+}
+
+function receiveAttack(cordX, cordY) {
+  if (!this.isValidCell(cordX, cordY)) return;
+  if (this.trackShot[cordY][cordX] !== 0) return;
+
+  if (this.board[cordY][cordX] === 1) {
+    this.board[cordY][cordX] = 2;
+    this.recordHit(cordX, cordY);
+    this.ships.forEach((ship) => {
+      const mainAxis = ship.axis === 'y' ? ship.cY : ship.cX;
+      const otherAxis = ship.axis === 'y' ? ship.cX : ship.cY;
+      const boardMain = ship.axis === 'y' ? cordY : cordX;
+      const boardOther = ship.axis === 'y' ? cordX : cordY;
+      const mainEnd = mainAxis + ship.ship.length - 1;
+
+      if (
+        mainAxis <= boardMain &&
+        boardMain <= mainEnd &&
+        otherAxis === boardOther
+      ) {
+        ship.ship.hit(boardMain - mainAxis);
+      }
+    });
+  } else {
+    this.trackShot[cordY][cordX] = 1;
+  }
+}
+
+function isGameOver() {
+  return this.ships.every((ship) => ship.ship.isSunk());
+}
+
+Object.assign(GameBoard.prototype, {
+  addShip,
+  isCellEmpty,
+  receiveAttack,
+  isValidCell,
+  isPlacementPossible,
+  recordHit,
+  isGameOver,
+});
+
+export default GameBoard;
---
+++ src/js/factories/tests/game-board.test.js
@@ -0,0 +1,115 @@
+import GameBoard from '../game-board';
+
+describe('GameBoard', () => {
+  let gameBoard;
+  const testBoard = [];
+
+  beforeEach(() => {
+    gameBoard = new GameBoard(10, 10);
+    // ship = new Ship(3);
+    for (let i = 0; i < 10; i++) {
+      testBoard.push([]);
+      for (let j = 0; j < 10; j++) {
+        testBoard[i].push(0);
+      }
+    }
+  });
+
+  afterEach(() => {
+    testBoard.length = 0;
+  });
+
+  test('should be able to be define', () => {
+    expect(gameBoard).toBeDefined();
+  });
+
+  test('should be able to initialize board', () => {
+    expect(gameBoard.board).toEqual(testBoard);
+    expect(gameBoard.trackShot).toEqual(testBoard);
+  });
+
+  test('test for valid cell', () => {
+    expect(gameBoard.isValidCell(0, 0)).toEqual(true);
+    expect(gameBoard.isValidCell(10, 10)).toEqual(false);
+    expect(gameBoard.isValidCell(10, 0)).toEqual(false);
+    expect(gameBoard.isValidCell(0, 10)).toEqual(false);
+    expect(gameBoard.isValidCell(-1, 0)).toEqual(false);
+  });
+
+  test('game board should be able to add items', () => {
+    gameBoard.addShip(2, 0, 0, 'x');
+    expect(gameBoard.ships.length).toEqual(1);
+  });
+
+  test('ships should not overlap each other', () => {
+    gameBoard.addShip(2, 0, 0, 'x');
+    gameBoard.addShip(4, 1, 1, 'y'); // neighbor
+    expect(gameBoard.ships.length).toEqual(1);
+  });
+
+  test('should not be able to place outside the board', () => {
+    gameBoard.addShip(2, 1, 6, 'x'); // valid
+    gameBoard.addShip(3, 9, 0, 'x'); // valid
+    gameBoard.addShip(3, 0, 9, 'y'); // valid but neighbor exists
+    gameBoard.addShip(2, 0, 9, 'y'); // valid
+    gameBoard.addShip(2, 10, 0, 'x'); // invalid
+    gameBoard.addShip(2, 0, 10, 'x'); // invalid
+    gameBoard.addShip(2, 0, 10, 'x'); // invalid
+    gameBoard.addShip(2, 0, 10, 'y'); // invalid
+    gameBoard.addShip(2, -1, 0, 'y'); // invalid
+    expect(gameBoard.ships.length).toEqual(3);
+  });
+
+  test('ship should track the hit', () => {
+    gameBoard.addShip(2, 8, 0, 'x'); // valid
+    gameBoard.addShip(3, 9, 1, 'x'); // valid but neighbor exists
+    gameBoard.addShip(3, 9, 9, 'y'); // valid
+    gameBoard.ships[1].ship.hit(2);
+    expect(gameBoard.ships[1]).toEqual({
+      ship: { hits: [2], length: 3 },
+      axis: 'y',
+      cX: 9,
+      cY: 7,
+    });
+  });
+
+  test('should be able to track Hits', () => {
+    gameBoard.addShip(2, 8, 0, 'x'); // valid 0 x7-9 y0
+    gameBoard.addShip(3, 9, 2, 'x'); // valid 1 x7-9 y2
+    gameBoard.addShip(3, 0, 9, 'y'); // valid 2 x0 y7-9
+    gameBoard.receiveAttack(7, 2); // hit 1
+    gameBoard.receiveAttack(7, 1); // miss
+    gameBoard.receiveAttack(0, 9); // hit 2
+    gameBoard.receiveAttack(0, 7); // hit 2
+    gameBoard.receiveAttack(7, 3); // missed
+    expect(gameBoard.board[1][7]).toEqual(0); // miss
+    expect(gameBoard.board[2][7]).toEqual(2); // hit
+    expect(gameBoard.trackShot[1][7]).toEqual(1); // miss
+    expect(gameBoard.trackShot[3][8]).toEqual(-1); // neighbor corner of hit
+    expect(gameBoard.trackShot[2][7]).toEqual(2); // missed
+    expect(gameBoard.ships[0].ship.hits.length).toEqual(0);
+    expect(gameBoard.ships[1].ship.hits.length).toEqual(1);
+    expect(gameBoard.ships[1].ship.hits).toEqual([0]);
+    expect(gameBoard.ships[2].ship.hits.length).toEqual(2);
+    expect(gameBoard.ships[2].ship.hits.sort()).toEqual([0, 2].sort());
+  });
+
+  test('should be able to report if all ships sunk', () => {
+    gameBoard.addShip(2, 8, 0, 'x'); // valid 0 x7-9 y0
+    gameBoard.addShip(2, 9, 2, 'x'); // valid 1 x7-9 y2
+    gameBoard.receiveAttack(8, 0); // hit 0
+    gameBoard.receiveAttack(9, 0); // hit 0
+    gameBoard.receiveAttack(8, 2); // hit 1
+    expect(gameBoard.isGameOver()).toEqual(false);
+  });
+
+  test('should be able to report if all ships sunk', () => {
+    gameBoard.addShip(2, 8, 0, 'x'); // valid 0 x7-9 y0
+    gameBoard.addShip(2, 9, 2, 'x'); // valid 1 x7-9 y2
+    gameBoard.receiveAttack(8, 0); // hit 0
+    gameBoard.receiveAttack(9, 0); // hit 0
+    gameBoard.receiveAttack(8, 2); // hit 1
+    gameBoard.receiveAttack(9, 2); // hit 1
+    expect(gameBoard.isGameOver()).toEqual(true);
+  });
+});
