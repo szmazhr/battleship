ed7ed2f0b214a90cf804367ff80750d02e40d2b6
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-06-07 19:00:27 +0530

    deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './scss/index.scss';
-import eventAggregator from './js/modules/event-aggregator';
-import './js/even-subcribers';
-
-eventAggregator.publish('startApp');
-
-// temporary
-// eventAggregator.publish('start-button-clicked');
